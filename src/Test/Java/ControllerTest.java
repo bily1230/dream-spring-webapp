8ea7d410fa1d34a773e2cba8caef68025c6f6622
bilynb <1126729841@qq .com>
2017-08-27 23:37:31 +0800

    问题

---
+++ src/Test/Java/ControllerTest.java
@@ -0,0 +1,28 @@
+import com.dream.spring.controller.HelloWord;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+
+/**
+ * Created by ning on 2017/8/27.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class ControllerTest {
+
+    @Test
+    public void testHomePage(){
+        HelloWord helloWord = new HelloWord();
+        MockMvc mockMvc = standaloneSetup(helloWord).build();
+        mockMvc.perform(get("/helloWord/readWord"))
+                .andExpect(view().name("return"));
+    }
+}
