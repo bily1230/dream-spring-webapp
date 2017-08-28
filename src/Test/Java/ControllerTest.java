07ad84c0d79c9004a2e611e3c7660314715079ae
bilynb <1126729841@qq .com>
2017-08-28 23:43:32 +0800

    问题

--- src/Test/Java/ControllerTest.java
+++ src/Test/Java/ControllerTest.java
@@ -1,11 +1,19 @@
 import com.dream.spring.controller.HelloWord;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import  org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
@@ -15,14 +23,28 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
  * Created by ning on 2017/8/27.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
+@WebAppConfiguration("com.dream.spring.controller")
+//@ContextConfiguration({"classpath*:/applicationContext.xml" })
+@ContextConfiguration(classes = {WebConfig.class,RootConfig.class})
 public class ControllerTest {
 
+    MockMvc mockMvc;
+    @Autowired
+    HelloWord helloWord;
+
+    @Before
+    public void setUp(){
+        mockMvc = standaloneSetup(helloWord).build();
+    }
     @Test
-    public void testHomePage(){
-        HelloWord helloWord = new HelloWord();
-        MockMvc mockMvc = standaloneSetup(helloWord).build();
-        mockMvc.perform(get("/helloWord/readWord"))
-                .andExpect(view().name("return"));
+    public void testHomePage() throws Exception {
+
+   mockMvc.perform(MockMvcRequestBuilders.get("/helloWord/521521/readWord?name=lihan")
+                .param("age","14"))
+           .andExpect(view().name("return"))
+           .andExpect(model().attribute("name","小寒"));
+
+
+
     }
 }
--- src/main/java/com/dream/spring/controller/HelloWord.java
+++ src/main/java/com/dream/spring/controller/HelloWord.java
@@ -1,10 +1,15 @@
 package com.dream.spring.controller;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
 
@@ -16,10 +21,12 @@ import java.lang.reflect.Method;
 @RequestMapping("helloWord")
 public class HelloWord {
 
-    @RequestMapping(value="/readWord" ,method = RequestMethod.GET)
-    public String readWord(){
+    @RequestMapping(value="/{id}/readWord" ,method = RequestMethod.GET)
+    public String readWord(@PathVariable("id") String id, @RequestParam("name")String name,
+                           Model model, HttpServletRequest request){
 
-        System.out.println("中国梦！作为中国人");
+        System.out.println("中国梦！作为中国人"+id+"---"+name+"--"+request.getParameter("age"));
+        model.addAttribute("name","小寒");
         return "return";
     }
 }
