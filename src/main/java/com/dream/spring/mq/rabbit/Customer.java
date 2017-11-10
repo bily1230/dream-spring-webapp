2f767c2a579ab247bc2c628b87d1aa96d56dfaae
宁彬 <ningbing@homolo.com>
2017-11-10 15:58:29 +0800

    rabbitmq

--- src/main/java/com/dream/spring/mq/rabbit/Customer.java
+++ src/main/java/com/dream/spring/mq/rabbit/Customer.java
@@ -1,19 +1,50 @@
 package com.dream.spring.mq.rabbit;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import spring.RabbitConfiguration;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+
 
 public class Customer {
-
-	public static void main(String[] args) {
-		ApplicationContext context =
-		        new AnnotationConfigApplicationContext(RabbitConfiguration.class);
-		RabbitTemplate rabbitTemplate = context.getBean(RabbitTemplate.class);
-		Object name = rabbitTemplate.convertSendAndReceive("myqueue");
-		System.out.print("recesiver:"+name.toString());
+	private final static String QUEUE_NAME = "myqueue";
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();    
+        factory.setHost("localhost");    
+        //指定用户 密码  
+        factory.setUsername("guest");  
+        factory.setPassword("guest");  
+        //指定端口  
+        factory.setPort(AMQP.PROTOCOL.PORT);
+		 	
+		 Connection connection = factory.newConnection();
+		 Channel channel = connection.createChannel();
+		 
+		 boolean durable = true;
+		 
+		 channel.queueDeclare(QUEUE_NAME, durable,  false, false, null);
+		
+		//公平转发  设置最大服务转发消息数量    只有在消费者空闲的时候会发送下一条信息。  
+        
+        
+        final Consumer consumer = new DefaultConsumer(channel) {  
+            @Override  
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {  
+              String message = new String(body, "UTF-8");  
+              System.out.println(" [x] Received ["  + envelope.getRoutingKey() + "] :'" + message + "'");  
+            }  
+          };  
+          channel.basicConsume(QUEUE_NAME, true, consumer);  
+	}  
 	}
 
-}
--- src/main/java/com/dream/spring/mq/rabbit/Producter.java
+++ src/main/java/com/dream/spring/mq/rabbit/Producter.java
@@ -2,22 +2,45 @@ package com.dream.spring.mq.rabbit;
 
 
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
-import spring.RabbitConfiguration;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
 
 
 public class Producter {
-
-	public static void main(String[] args) {
-		 ApplicationContext context =
-			        new AnnotationConfigApplicationContext(RabbitConfiguration.class);
-		 RabbitTemplate rabbitTemplate = context.getBean(RabbitTemplate.class);
+	
+	private final static String QUEUE_NAME = "myqueue";
+	
+	public static void main(String[] args) throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();    
+        factory.setHost("localhost");    
+        //指定用户 密码  
+        factory.setUsername("guest");  
+        factory.setPassword("guest");  
+        //指定端口  
+        factory.setPort(AMQP.PROTOCOL.PORT);
+		 	
+		 Connection connection = factory.newConnection();
+		 Channel channel = connection.createChannel();
 		 
-		 	rabbitTemplate.convertAndSend("Hello World");
-		    System.out.println("Sent: Hello World");
+		 boolean durable = true;
+		 
+		 channel.queueDeclare(QUEUE_NAME, durable,  false, false, null);
+		 for(int i =0;i<5;i++){
+			 String message = i + "hellpword";
+			 channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
+			 System.out.println("Sent: "+message);
+		 }
+		 
+		channel.close();
+		connection.close();
 
 	}
 
